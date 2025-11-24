f6b2381cea9b53300f87285782e9a39976dc1bbb
Breno <srbentu@gmail.con>
2025-11-24 22:51:42 +0100

    Refactor code structure for improved readability and maintainability

---
+++ pages/three/index.js
@@ -0,0 +1,65 @@
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+
+export default function ThreePage() {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    // Canvas
+    const canvas = canvasRef.current;
+
+    // Scene
+    const scene = new THREE.Scene();
+
+    /**
+     * Object
+     */
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(0.7, -0.6, 1);
+    scene.add(mesh);
+
+    //axes helper
+    const axesHelper = new THREE.AxesHelper();
+    scene.add(axesHelper);
+    /**
+     * Sizes
+     */
+    const sizes = {
+      width: 800,
+      height: 600,
+    };
+
+    /**
+     * Camera
+     */
+    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+    camera.position.z = 3;
+    scene.add(camera);
+
+    /**
+     * Renderer
+     */
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvas,
+    });
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.render(scene, camera);
+
+    // Cleanup
+    return () => {
+      geometry.dispose();
+      material.dispose();
+      renderer.dispose();
+    };
+  }, []);
+
+  return (
+    <>
+      <canvas ref={canvasRef} className="webgl" />
+    </>
+  );
+}
